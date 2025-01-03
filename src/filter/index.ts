fad097669233e4311502ec339d5e56839f1077d2
tardis <14971857@qq.com>
2025-01-03 16:00:37 +0800

    refactor: change URL

--- src/filter/index.ts
+++ src/filter/index.ts
@@ -5,6 +5,7 @@ import { HtmlPath, PLUGIN_NAME } from '@/constants';
 import { PluginConfig } from '@/types';
 import * as process from 'node:process';
 
+// img.shields.io
 const getHtmlContent = (content?: string) => {
   return `<link rel="stylesheet" href="/${PLUGIN_NAME}/${HtmlPath.CSS}">
 ${
@@ -12,7 +13,7 @@ ${
   `<div class="post-gemini-ai">
   <img
     class="no-lightbox ai-summary-img"
-    src="https://api-shields.edui.fun/badge/Gemini-文章摘要-blue.svg?logo=data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgMjQgMjQiPjxwYXRoIGZpbGw9Im5vbmUiIHN0cm9rZT0iI2ZmZmZmZiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBzdHJva2Utd2lkdGg9IjIiIGQ9Im0yMS42NCAzLjY0bC0xLjI4LTEuMjhhMS4yMSAxLjIxIDAgMCAwLTEuNzIgMEwyLjM2IDE4LjY0YTEuMjEgMS4yMSAwIDAgMCAwIDEuNzJsMS4yOCAxLjI4YTEuMiAxLjIgMCAwIDAgMS43MiAwTDIxLjY0IDUuMzZhMS4yIDEuMiAwIDAgMCAwLTEuNzJNMTQgN2wzIDNNNSA2djRtMTQgNHY0TTEwIDJ2Mk03IDhIM20xOCA4aC00TTExIDNIOSIvPjwvc3ZnPg=="
+    src="https://static.ksh7.com/utils/gemini-summary.svg"
   />
 </div>`
 }
