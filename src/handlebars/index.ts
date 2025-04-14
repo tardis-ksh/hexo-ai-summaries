ab8101fd46e488a5cb4153d4c21a051e618e1204
tardis <14971857@qq.com>
2025-04-14 16:26:13 +0800

    feat: support or

--- src/handlebars/index.ts
+++ src/handlebars/index.ts
@@ -13,6 +13,14 @@ handlebars.registerHelper('if_eq', function (a, b, opts) {
   return opts.inverse(this);
 });
 
+// ifOr
+handlebars.registerHelper('ifOr', function (arg1, arg2, options) {
+  if (arg1 || arg2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 const generateTemplate = (content: string, config: Record<string, any>) => {
   const template = handlebars.compile(content, { noEscape: true });
   return template(config);
